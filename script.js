8b5a814da93424e05f338353176e7ec5c45035bf
ferari-shafi007 <ferari.shafi007@gmail.com>
2026-02-18 20:28:15 +0600

    locomotive

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const locomotiveScroll = new LocomotiveScroll();
+document.addEventListener("DOMContentLoaded", function () {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"), // The main container element
+    smooth: true, // Enable smooth scrolling
+    // Add other options as needed, e.g., multiplier, smoothMobile
+  });
+});
