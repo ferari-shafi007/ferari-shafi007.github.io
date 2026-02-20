212bc5b081c913b6eb28b9fb6128c243fbf733ba
ferari-shafi007 <ferari.shafi007@gmail.com>
2026-02-20 19:23:05 +0600

    20206-02-20

--- assets/mouseTail.js
+++ assets/mouseTail.js
@@ -1,8 +1,8 @@
 let mouseTail = document.querySelector("#mouseTail");
-function moveMouseTail(e) {
+
+document.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
   mouseTail.style.left = x + "px";
   mouseTail.style.top = y + "px";
-}
-document.addEventListener("mousemove", moveMouseTail);
+});
