0a15adb917225b5a71ef6830d1bf83b861ba4f0d
ferari-shafi007 <ferari.shafi007@gmail.com>
2026-02-24 12:31:42 +0600

    feat: Add textured cylinder to Three.js scene and remove cube

--- assets/threejs.js
+++ assets/threejs.js
@@ -5,18 +5,27 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x7bff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new THREE.TextureLoader();
+
+const texture = loader.load("./assets/img/Mesa de trabajo 2.png");
+texture.colorSpace = THREE.SRGBColorSpace;
+
+const geometry = new THREE.CylinderGeometry(1.5, 1.5, 1.5, 64, 1, false);
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const cylinder = new THREE.Mesh(geometry, material);
+scene.add(cylinder);
 let canvas = document.querySelector("#draw");
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  alpha: true,
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 function animate(time) {
   renderer.render(scene, camera);
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
+  // cylinder.rotation.x = time / 2000;
+  cylinder.rotation.y = time / 5000;
 }
 renderer.setAnimationLoop(animate);
